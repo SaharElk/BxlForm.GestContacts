b8ec9e26dc4c2b9b0033319a4f168b6e5a9cd6ae
TMCognitic <thierry.morre@cognitic.be>
2021-06-07 17:15:40 +0200

    Implémentations des services

---
+++ BxlForm.GestContacts.Models.Global/Services/CategoriesService.cs
@@ -0,0 +1,41 @@
+﻿using BxlForm.GestContacts.Models.Global.Data;
+using BxlForm.GestContacts.Models.Global.Mappers;
+using BxlForm.Tools.Connections.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BxlForm.GestContacts.Models.Global.Services
+{
+    public class CategoriesService
+    {
+        private readonly Connection _connection;
+
+        public CategoriesService(Connection connection)
+        {
+            _connection = connection;
+        }
+
+
+        public IEnumerable<Category> Get()
+        {
+            Command command = new Command("SELECT [Id], [Name] FROM [Category];", false);            
+            return _connection.ExecuteReader(command, dr => dr.ToCategory());
+        }
+
+        public Category Get(int id)
+        {
+            Command command = new Command("SELECT [Id], [Name] FROM [Category] WHERE [Id] = @Id;", false);
+            command.AddParameter("Id", id);
+            return _connection.ExecuteReader(command, dr => dr.ToCategory()).SingleOrDefault();
+        }
+
+        public bool Insert(Category category)
+        {
+            Command command = new Command("BFSP_AddCategory", true);
+            command.AddParameter("Name", category.Name);
+            return _connection.ExecuteNonQuery(command) == 1;
+        }
+    }
+}
---
+++ BxlForm.GestContacts.Models.Global/Services/ContactsService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BxlForm.GestContacts.Models.Global.Data;
+using BxlForm.GestContacts.Models.Global.Mappers;
+using BxlForm.Tools.Connections.Database;
+
+namespace BxlForm.GestContacts.Models.Global.Services
+{
+    public class ContactsService
+    {
+        private readonly Connection _connection;
+
+        public ContactsService(Connection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Contact> Get()
+        {
+            Command command = new Command("SELECT [Id], [LastName], [FirstName], [Email], [CategoryId] FROM [Contact];", false);
+            return _connection.ExecuteReader(command, dr => dr.ToContact());
+        }
+
+        public IEnumerable<Contact> GetByCategory(int id)
+        {
+            Command command = new Command("SELECT [Id], [LastName], [FirstName], [Email], [CategoryId] FROM [Contact] WHERE [CategoryId] = @CategoryId;", false);
+            command.AddParameter("CategoryId", id);
+            return _connection.ExecuteReader(command, dr => dr.ToContact());
+        }
+
+        public Contact Get(int id)
+        {
+            Command command = new Command("SELECT [Id], [LastName], [FirstName], [Email], [CategoryId] FROM [Contact] WHERE [Id] = @Id;", false);
+            command.AddParameter("Id", id);
+            return _connection.ExecuteReader(command, dr => dr.ToContact()).SingleOrDefault();
+        }
+
+        public bool Insert(Contact contact)
+        {
+            Command command = new Command("BFSP_AddContact", true);
+            command.AddParameter("LastName", contact.LastName);
+            command.AddParameter("FirstName", contact.FirstName);
+            command.AddParameter("Email", contact.Email);
+            command.AddParameter("CategoryId", contact.CategoryId);
+            return _connection.ExecuteNonQuery(command) == 1;
+        }
+
+        public bool Update(int id, Contact contact)
+        {
+            Command command = new Command("BFSP_UpdateContact", true);
+            command.AddParameter("Id", id);
+            command.AddParameter("LastName", contact.LastName);
+            command.AddParameter("FirstName", contact.FirstName);
+            command.AddParameter("Email", contact.Email);
+            command.AddParameter("CategoryId", contact.CategoryId);
+            return _connection.ExecuteNonQuery(command) == 1;
+        }
+
+        public bool Delete(int id)
+        {
+            Command command = new Command("BFSP_DeleteContact", true);
+            command.AddParameter("Id", id);
+            return _connection.ExecuteNonQuery(command) == 1;
+        }
+    }
+}
