db3f2dd269e8f22cc40e01cf41d48b401dd0a236
TMCognitic <thierry.morre@cognitic.be>
2021-06-07 16:06:52 +0200

    Add Extensions Methods for IDataRecord

---
+++ BxlForm.GestContacts.Models.Global/Mappers/DataRecord.cs
@@ -0,0 +1,32 @@
+﻿using BxlForm.GestContacts.Models.Global.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace BxlForm.GestContacts.Models.Global.Mappers
+{
+    internal static class DataRecord
+    {
+        internal static Contact ToContact(this IDataRecord record)
+        {
+            return new Contact()
+            {
+                Id = (int)record["Id"],
+                LastName = (string)record["LastName"],
+                FirstName = (string)record["FirstName"],
+                Email = (string)record["Email"],
+                CategoryId = (int)record["CategoryId"]
+            };
+        }
+
+        internal static Category ToCategory(this IDataRecord record)
+        {
+            return new Category()
+            {
+                Id = (int)record["Id"],
+                Name = (string)record["Name"],                
+            };
+        }
+    }
+}
